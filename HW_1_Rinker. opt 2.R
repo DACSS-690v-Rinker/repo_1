1f6f3648488a6c5fde1085637c4f257223e258ea
diannnay <dianarinker@gmail.com>
2024-12-30 17:44:44 -0500

    updates

--- HW_1_Rinker. opt 2.R
+++ HW_1_Rinker. opt 2.R
@@ -11,12 +11,13 @@ library(readr)
 library(ggplot2)
 library(magrittr)
 library(DescTools)
+library(ggpubr)
 rm(list = ls()) # clean memory
 # Loading data ----------------------
 location='https://github.com/DACSS-Visual/tabular_univar_cat/raw/main/data/'
 file='eduwa.rda'
 link=paste0(location,file)
-
+write.csv(eduwa, file = "eduwa_data.csv", row.names = FALSE)
 
 #getting the data TABLE from the file in the cloud:
 load(file=url(link))
@@ -74,8 +75,9 @@ md=Median(eduwa$Student.Teacher.Ratio,na.rm = T)
 mn=Mean(eduwa$Student.Teacher.Ratio,na.rm = T)
 
 # Mean and median are very close, but since data has a lot of outliers,  I will display the median.
-# Annotating: 
 
+
+# Adding median and annotating: 
 txt_md=paste0('#Median: ',md)
 
 b1_vertical = b1 + 
@@ -101,8 +103,8 @@ b1_annot= b1_vertical +
 
 # Computing amount of outliers: 
 
-txtUpper=paste0('Threshold: ',upperT)
-txtLower=paste0('Threshold: ',lowerT)
+txtUpper=paste0('Threshold:\n ',upperT)
+txtLower=paste0('Threshold:\n ',lowerT)
 
 (numOutliers_h=sum(eduwa$Student.Teacher.Ratio>upperT,na.rm = T))
 (numOutliers_l=sum(eduwa$Student.Teacher.Ratio<lowerT,na.rm = T))
@@ -153,7 +155,6 @@ b2_annot= b2_annot +
                    ,size =3)
 b2_annot
 
-
 # Removing values in the vertical axis and customizing the grid: 
 
 b2_annot_noX = b2_annot + 
@@ -161,17 +162,15 @@ b2_annot_noX = b2_annot +
   theme(axis.text.y = element_blank(),
               axis.ticks.y = element_blank(),
               axis.title.y = element_blank()) 
-
 b2_annot_noX
 
-# Customizing the text: 
-
-b1_newGrid_axisText = b1_annot_noX+ 
+# Customizing the text on x-axis: 
+b2_newGrid_axisText = b2_annot_noX + 
                       theme(axis.text.x = element_text(angle = 60,
                                                        size = 10,
                                                        vjust = 0.5)
                             )
-b1_newGrid_axisText
+b2_newGrid_axisText
 
 # Statistical calculations: ---------------
 # Supplementing summary with measures of spread and dispersion: 
@@ -197,9 +196,11 @@ Kurt(eduwa$Student.Teacher.Ratio,
 sk=Skew(eduwa$Student.Teacher.Ratio,
         na.rm = T)
 
+# This data us hughly concentrated around the center and skewed to the right. 
 
 # Histogram: -------------------------
-barWIDTH=50
+range(eduwa$Student.Teacher.Ratio)
+barWIDTH=2
 library(ggplot2)
 base= ggplot(eduwa)  
 h1= base + 
@@ -207,64 +208,107 @@ h1= base +
                       binwidth = barWIDTH,
                       fill='brown') 
 h1= h1 + 
-    labs(y="count")
-
+    labs(y="Count")
 h1
-# Adding annotations: 
 
-# texts
+# Adding annotations: 
+# Texts
 txtMean=paste0('Mean:',round(mn))
 txtSkew=paste0('Skewness:',round(sk,2))
 
-
 h1_ann = h1+
-        geom_vline(xintercept = mn,color='red') + # mean as line
+        geom_vline(xintercept = mn, color='black') + # mean as line
   # about the mean
          annotate(geom = 'text',color='red',
                  label=txtMean, # mean as text
                  y = 400,
-                 x=mn+5,
+                 x=mn+6,
                  angle=90) + 
   # about the skewness
           annotate(geom = 'text', color='blue',
            label=txtSkew, # skewness as text
            y = 50,
-           x=upperT+170,
+           x=upperT+20,
            angle=0) 
 
 h1_ann 
 
+
 # Combining  histogram and boxplot in one visual: ---------------
 
 install.packages ("ggpubr")
-library(ggpubr)
 
-ggarrange(h1_ann,b1_newGrid_axisText,align='v',ncol = 1,heights = 2:4)
 
-# alternative violin plot: 
+ggarrange(b1_newGrid_axisText, h1_ann, align='v',ncol = 1,heights = 3:2)
+
+# Alternative violin plot: --------------------
+
 base=ggplot(eduwa, aes(x=0,y=Student.Teacher.Ratio))+
   theme_classic()
 
 vio=base+
-  geom_violin(trim=FALSE, fill="grey")
+  geom_violin(trim=FALSE, fill="grey")+
+  labs(title=titleText,
+        subtitle = sub_titleText,
+        # x =NULL,  #x.AxisText
+        y = NULL, #y.AxisText
+        caption = sourceText)
 
 viobox=vio+
-  geom_boxplot(width=0.1,
-               outlier.color = 'red',
-               colour = 'white',
-               fill='purple')
+       geom_boxplot(width=0.1,
+                     outlier.color = 'red',
+                     colour = 'white',
+                     fill='purple')
+
+viobox_ann=viobox + 
+          coord_flip() + 
+          theme(axis.text.y = element_blank(),
+                axis.ticks.y = element_blank(),
+                axis.title.y = element_blank()) +
+        annotate(geom = 'text',
+                label=txt_Outliers_h,
+                y = upperT+60,
+                x=0.1,color='red',
+                angle=0)+
+        annotate(geom = 'text',
+                label=txt_Outliers_l,
+                y = lowerT -5,
+                x=0.1,color='red',
+                angle=0)+
+        geom_hline(yintercept = upperT,
+                   color='purple',
+                   linetype="dotted",
+                   size=1)+
+        geom_hline(yintercept = lowerT,
+                   color='purple',
+                   linetype="dotted",
+                   size=1)+
+        annotate(geom = 'text',
+                 label=txtUpper,
+                 y = upperT+5,
+                 x=0.2,
+                 angle=0
+                 ,colour ='purple') +
+        annotate(geom = 'text',
+                 label=txtLower,
+                 y = lowerT-5,
+                 x=0.2,
+                 angle=0
+                 ,colour ='purple')+ # text angle# text angle 
+        geom_hline(yintercept = md,
+                     color='blue',
+                     linetype="dashed",
+                     size=1) + 
+          annotate(geom = 'text',
+                     label=txt_md,
+                     y = md-7,
+                     x=0.5,
+                     angle=0
+                     ,colour ='blue') # text angle
 
 
-viobox=viobox + coord_flip() + 
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.title.y = element_blank()) +
-  annotate(geom = 'text',
-         label=txtOutliers,
-         y = upperT+60,
-         x=0.1,color='red',
-         angle=0)
-viobox
+viobox_ann
+saveRDS(viobox_ann, file = "del1Draft.rds")
 
 
 
